acd7760ff4ae82198761a84c6e535d6463ff7fb5
kapilsoni9977 <kapil.soni.nt@gmail.com>
2022-04-01 10:52:32 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  dob: string;
+  ed: string;
+  gender: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    name: 'John Smith',
+    dob: '02/14/1988',
+    ed: '02/14/1988',
+    gender: 'M',
+  },
+  {
+    position: 2,
+    name: 'Jennifer McArthur',
+    dob: '02/14/1988',
+    ed: '02/14/1988',
+    gender: 'F',
+  },
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  displayedColumns: string[] = ['position', 'name', 'dob', 'ed', 'gender'];
+  dataSource = ELEMENT_DATA;
 }
